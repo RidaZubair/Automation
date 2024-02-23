4309ea45ebb34cafe6b9da82178e4fef58f6d735
RidaZubair <rzubair@anaconda.com>
2024-02-23 20:58:01 +0500

    Automation code

--- tests/pages/anaconda.org/login-page.ts
+++ tests/pages/anaconda.org/login-page.ts
@@ -1,11 +1,11 @@
-import { click, clickAndNavigate, fill, getLocatorByRole } from 'vasu-playwright-utils';
+import { click, clickAndNavigate, fill, getLocatorByRole, getLocatorByText } from 'vasu-playwright-utils';
 import { expectElementToBeVisible } from 'vasu-playwright-utils';
 import { InValidLogin, ValidLogin } from '@testdata/sauce-anaconda.org-test-data';
 
 const userName = '#username';
 const password = '#password';
 const loginButton = '#logInButton';
-const errorMessage = `//*[@class='alert-box danger']`;
+const errorMessage = () => getLocatorByText('Could not log you in');
 const dashboardMessage = () => getLocatorByRole('heading', { name: 'My Anaconda Landscape' });
 
 export async function loginWithValidCredentials(validCredentials = ValidLogin) {
@@ -19,5 +19,5 @@ export async function loginWithInvalidCredentials(invalidCredentials = InValidLo
   await fill(userName, invalidCredentials.username);
   await fill(password, invalidCredentials.password);
   await click(loginButton);
-  await expectElementToBeVisible(errorMessage);
+  await expectElementToBeVisible(errorMessage());
 }
